c6ebd85d6596d9712bd9541858c285fada636ba3
DominykasDriu <dominykas.driukas@gmail.com>
2021-04-03 13:31:26 +0300

    Added main components to App

--- flags-app/src/App.js
+++ flags-app/src/App.js
@@ -1,7 +1,13 @@
+import Filters from "./components/Filters";
+import Header from "./components/Header";
+import Output from "./components/Output";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Filters />
+      <Output />
     </div>
   );
 }
