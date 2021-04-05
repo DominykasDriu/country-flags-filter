69bb6dfe4a3d1f19a4389fb8efefaba172affe18
DominykasDriu <dominykas.driukas@gmail.com>
2021-04-05 16:49:52 +0300

    Added logic to single country info page

--- flags-app/src/components/FullCountryInfo.jsx
+++ flags-app/src/components/FullCountryInfo.jsx
@@ -1,51 +1,74 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router'
-import {BsArrowLeft} from 'react-icons/bs'
+import React, { useEffect, useState } from 'react'
+// import { useParams } from 'react-router'
+import { BsArrowLeft } from 'react-icons/bs'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 import './FullCountryInfo.scss'
 
 export default function FullCountryInfo() {
   let { id } = useParams()
+  const [info, setInfo] = useState([])
 
   useEffect(() => {
+    setInfo('loading')
     fetch(`https://restcountries.eu/rest/v2/alpha/${id}`)
     .then(res => res.json())
     .then(data => {
       console.log(data);
       // if (data.status === 404) return setCountries('error')
       // setCountries(data)
+      setInfo(data)
     })
     .catch(err => console.log(err))
   }, [id])
-
+  
   return (
     <div className='country-info-wrapper'>
-      <button className='back'><BsArrowLeft/>Back</button>
+      <Route>
+        <Link to='/'>
+          <button className='back'><BsArrowLeft/>Back</button>
+        </Link>
+      </Route>
+      {info === 'loading' ? <div className='loader'></div> :
+      
       <div className="info-wrapper">
-        <img src="https://restcountries.eu/data/alb.svg" alt=""/>
-        <div className="text-info">
-          <h2>Belgium</h2>
-          <div className="lists-wrapper">
-            <ul>
-              <li><span>Native name:</span> Belgium</li>
-              <li><span>Population:</span> 11111111</li>
-              <li><span>Region:</span> Western Europe</li>
-              <li><span>Sub region:</span> Western Europe</li>
-              <li><span>Capital:</span> Burssels</li>
-            </ul>
-            <ul>
-              <li><span>Top Level Domain:</span> .be</li>
-              <li><span>Currencies:</span> Euro</li>
-              <li><span>Languages:</span> Dutch, German</li>
-            </ul>
-          </div>
-          <div className="border-countries-wrapper">
-            <p>Border Countries:</p>
-            <button>France</button>
-            <button>Geramny</button>
-            <button>Netherlands</button>
-          </div>
+        <div className='loader'></div>
+      <img src={info.flag} alt=""/>
+      <div className="text-info">
+        <h2>{info.name}</h2>
+        <div className="lists-wrapper">
+          <ul>
+            <li><span>Native name:</span> {info.nativeName}</li>
+            <li><span>Population:</span> {info.population}</li>
+            <li><span>Region:</span> {info.region}</li>
+            <li><span>Sub region:</span> {info.subregion}</li>
+            <li><span>Capital:</span> {info.capital}</li>
+          </ul>
+          <ul>
+            <li><span>Top Level Domain:</span> {info.topLevelDomain}</li>
+            <li><span>Currencies:</span> {info.currencies ? info.currencies.map(e => e.name).join(', ') : ''}</li>
+            <li><span>Languages:</span> {info.languages ? info.languages.map(e => e.name).join(', ') : ''}</li>
+          </ul>
+        </div>
+        <div className="border-countries-wrapper">
+          <p>Border Countries:</p>
+          <Route>
+            {info.borders && info.borders.length > 1 ? info.borders.map(e => (
+              <Link to={e}>
+                <button>{e}</button>
+              </Link>
+            ))
+            : (<p style={{paddingLeft: '10px', fontWeight: 300}}>No countries have borders with {info.name}</p>)}
+          </Route>
         </div>
       </div>
+    </div>
+      }
     </div>
   )
 }
